94c2b32a55bb34c92bbfed4d16d336d5cffbccb9
Dino Source <sourcerer.cpp@gmail.com>
2022-06-29 16:07:58 +0400

    Add print outs in deadlock_ project

--- multithreading_/deadlock_/main.cpp
+++ multithreading_/deadlock_/main.cpp
@@ -15,28 +15,44 @@ namespace rkognitive {
 
     void showPoints(const std::vector<Point> & points) {
         mtx2.lock();
+        std::cout << "showPoints: mtx2.lock();\n";
+
         std::this_thread::sleep_for(std::chrono::milliseconds(1));
+
         mtx1.lock();
+        std::cout << "showPoints: mtx1.lock();\n";
+
         for (const auto & [x, y] : points) {
             std::cout << "x: " << x << ", y: " << y << std::endl;
             std::this_thread::sleep_for(std::chrono::milliseconds(10));
         }
+
         mtx1.unlock();
-        std::cout << std::endl;
+        std::cout << "showPoints: mtx1.unlock();\n";
+
         mtx2.unlock();
+        std::cout << "showPoints: mtx2.unlock();\n";
     }
 
     void spamMessageNTimes(const std::string & msg, const std::size_t n) {
         mtx1.lock();
+        std::cout << "spamMessageNTimes: mtx1.lock();\n";
+
         std::this_thread::sleep_for(std::chrono::milliseconds(1));
+
         mtx2.lock();
+        std::cout << "spamMessageNTimes: mtx2.lock();\n";
+
         for (std::size_t i = 0; i < n; ++i) {
             std::cout << msg;
             std::this_thread::sleep_for(std::chrono::milliseconds(5));
         }
+
         mtx2.unlock();
-        std::cout << std::endl;
+        std::cout << "spamMessageNTimes: mtx2.unlock();\n";
+
         mtx1.unlock();
+        std::cout << "spamMessageNTimes: mtx1.unlock();\n";
     }
 }
 
